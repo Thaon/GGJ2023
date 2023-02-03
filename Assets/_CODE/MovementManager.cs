24323b3ad462298e7432f060cd0caab82caa4c1a
YAP! <gabriele.maddaloni@gmail.com>
2023-02-03 20:09:10 +0000

    Made movement manager.

---
+++ Assets/_CODE/MovementManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    #region member variables
+
+    private Rigidbody _rb;
+
+    #endregion
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 dir, float speed)
+    {
+        dir = dir.normalized;
+        _rb.AddForce(dir * speed, ForceMode.Impulse);
+    }
+}
